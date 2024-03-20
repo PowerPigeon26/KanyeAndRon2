ea1d7bace697d5b4a9a4bd72248f5c93e5737b3d
Maverick Grayer <maverick72032@gmail.com>
2024-03-19 23:08:57 -0400

    API practice, place API calls in QuoteGenerator class

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeAndRon2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                QuoteGenerator.KanyeQuote();
+
+                QuoteGenerator.RonQuote();
+            }
+        }
+    }
+}
---
+++ QuoteGenerator.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanyeAndRon2
+{
+    internal class QuoteGenerator
+    {
+        public static void KanyeQuote()
+        {
+            var client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"---------------------------------------------");
+            Console.WriteLine($"Kanye: {kanyeQuote}");
+        }
+
+        public static void RonQuote()
+        {
+            var client = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine($"---------------------------------------------");
+            Console.WriteLine($"Ron: {ronQuote}");
+        }
+    }
+}
